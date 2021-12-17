1a4c6061b86dede9b2b28c8621ae8575be7060ac
mojtaba beheshti <moji2002@gmail.com>
2021-12-17 19:17:19 +0330

    remove extra packages

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,11 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { nanoid } from "nanoid";
 import connectDB from "../../middleware/mongodb";
 import Contact from "../../model/Contact";
-
-
-
 
 const API_KEY = process.env.API_KEY;
 
@@ -13,17 +8,14 @@ const handler =async (req, res)=> {
 
   try {
     if (req.method === "POST" && req.body.email) {
-      // db.set(nanoid(), { ...req.body, date: Date.now() });
       await Contact.create({ ...req.body });
       return res.status(200).send();
     }
-    // if (req.method === "GET" && req.headers.token === API_KEY) {
+    if (req.method === "GET" && req.headers.token === API_KEY) {
 
-    if (req.method === "GET") {
       const result = await Contact.find();
       return res.status(200).json(result);
 
-      // return res.status(200).json(db.JSON());
     }
   } catch (error) {
     return res.status(400).json(error.message);
