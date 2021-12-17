228828015c54f419ed4bf98f4da06ebb4e4bb8e4
mojtaba beheshti <moji2002@gmail.com>
2021-12-17 15:03:26 +0330

    add toast and make form interactive

--- components/home/Contact.jsx
+++ components/home/Contact.jsx
@@ -1,9 +1,53 @@
 import Image from "next/image";
 import s from "./contact.module.scss";
+import toast from "react-hot-toast";
+import { useRef, useState } from "react";
+import validateEmail from "../../lib/validateEmail";
 
 const Contact = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const contentRef = useRef();
+
+  const initialState = { value: "", error: false };
+
+  const [nameInput, setNameInput] = useState(initialState);
+  const [emailInput, setEmailInput] = useState(initialState);
+  const [contentInput, setContentInput] = useState(initialState);
+
+  const handleSendClick = async () => {
+    if (nameInput.value.length < 3) {
+      setNameInput((prev) => ({ ...prev, error: true }));
+      return toast("What was your name?");
+    }
+
+    if (!validateEmail(emailInput.value)) {
+      setEmailInput((prev) => ({ ...prev, error: true }));
+      return toast("Check your Email please!");
+    }
+
+    if (contentInput.value.length < 5) {
+      setContentInput((prev) => ({ ...prev, error: true }));
+      return toast("Just that?");
+    }
+    const myPromise = new Promise((resolve) => {
+      setTimeout(() => resolve("hello"), 1000);
+    });
+
+    toast.promise(myPromise, {
+      loading: "Loading",
+      success: "Got the data",
+      error: "Error when fetching",
+    });
+
+    // clear inputs
+    setNameInput((prev) => ({ ...prev, value: "" }));
+    setEmailInput((prev) => ({ ...prev, value: "" }));
+    setContentInput((prev) => ({ ...prev, value: "" }));
+  };
+
   return (
-    <section id='contact' className={s.wrapper}>
+    <section id="contact" className={s.wrapper}>
       <div className={s.inner}>
         <div className={s.leftHand}>
           <h3 className={s.text1}>Let&apos;s</h3>
@@ -14,11 +58,28 @@ const Contact = () => {
           </div>
         </div>
         <div className={s.rightHand}>
-          <Input label="Your name:" />
-          <Input label="Your email address:" />
-
-          <Input label="Content:" />
-          <SendButton />
+          <Input
+            inputRef={nameRef}
+            error={nameInput.error}
+            label="Your name:"
+            setInput={setNameInput}
+            value={nameInput.value}
+          />
+          <Input
+            inputRef={emailRef}
+            error={emailInput.error}
+            label="Your email address:"
+            setInput={setEmailInput}
+            value={emailInput.value}
+          />
+          <Input
+            inputRef={contentRef}
+            error={contentInput.error}
+            label="Content:"
+            setInput={setContentInput}
+            value={contentInput.value}
+          />
+          <SendButton onClick={handleSendClick} />
         </div>
       </div>
     </section>
@@ -35,20 +96,25 @@ const SocialIcon = ({ icon }) => {
   );
 };
 
-const Input = ({ label }) => {
+const Input = ({ label, inputRef, error, setInput, value }) => {
   return (
     <div>
       <div className={s.inputLabel}>{label}</div>
-
-      <input className={s.input} type="text" />
+      <input
+        ref={inputRef}
+        value={value}
+        className={`${s.input} ${error && s.inputError}`}
+        type="text"
+        onChange={(e) => setInput({ value: e.target.value, error: false })}
+      />
     </div>
   );
 };
 
-const SendButton = () => {
+const SendButton = ({ onClick }) => {
   return (
-    <button className={s.sendButton}>
-      <div className= {s.sendButtonText}>Send</div>
+    <button onClick={onClick} className={s.sendButton}>
+      <div className={s.sendButtonText}>Send</div>
       <Image src="/icons/arrow.svg" alt="send" width={30} height={30} />
     </button>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const toasterStyle = {
+    background: "#676cdb",
+    fontSize: 20,
+    padding: "40px 90px",
+    borderRadius: 0,
+    color: "#fff",
+  };
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster
+        toastOptions={{
+          style: toasterStyle,
+        }}
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
