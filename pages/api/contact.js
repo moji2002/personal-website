6e95d6b07562d4c3ec16ca24422453691b6fd4ec
mojtaba beheshti <moji2002@gmail.com>
2021-12-17 16:23:38 +0330

    test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -9,24 +9,30 @@ import { nanoid } from "nanoid";
 const API_KEY = process.env.API_KEY;
 
 export default async function handler(req, res) {
-  if (req.method === "POST" && req.body.email) {
-    // db.set(nanoid(), { ...req.body, date: Date.now() });
-    await prisma.contact.create({
-      data: {
-        ...req.body,
-        id: nanoid(),
-      },
-    });
-    return res.status(200).send();
-  }
-  if (req.method === "GET" && req.headers.token === API_KEY) {
+  try {
+    if (req.method === "POST" && req.body.email) {
+      // db.set(nanoid(), { ...req.body, date: Date.now() });
+      await prisma.contact.create({
+        data: {
+          ...req.body,
+          id: nanoid(),
+        },
+      });
+      return res.status(200).send();
+    }
+    // if (req.method === "GET" && req.headers.token === API_KEY) {
+  
+    if (req.method === "GET") {
+      const result = await prisma.contact.findMany();
+      return res.status(200).json(result);
+  
+      // return res.status(200).json(db.JSON());
+    }
+  } catch (error) {
+    return res.status(400).json(error);
 
-  // if (req.method === "GET") {
-    const result = await prisma.contact.findMany();
-    return res.status(200).json(result);
-
-    // return res.status(200).json(db.JSON());
   }
+
 
   return res.status(400).send();
 }
